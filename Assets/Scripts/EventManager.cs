40c52cd25fb7547df836d578375f20e46eb0809f
Kaho Neki <shinyzorua6@gmail.com>
2026-01-22 06:12:43 +0000

    over-the-shoulder camera and raycasting for grapple

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+//Static class for global access to events (note: C# actions, not unity events - this is more efficient)
+//Call EventManager.[event name] += foo; and EventManager.[event name].Invoke(params...);
+//(^where foo is a function with the signature void foo(params...);)
+//Note: c# actions aren't automatically unsubscribed upon destruction, make sure to manually unsubscribe with EventManager.[event name] -= foo; to avoid mem leak
+//Subscription and unsubscription will usually be handled in OnEnable() and OnDisable() respectively
+public static class EventManager
+{
+    //Triggered when the player has initiated a grapple to a GameObject (todo: better if GameObject or Transform?)
+    //todo: maybe unnecessary
+    public static Action<GameObject> OnGrapple;
+}
