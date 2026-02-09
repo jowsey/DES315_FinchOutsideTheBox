d8de63f6e98cc339c60abecc4a9b37dd21aa9fef
Kaho Neki <shinyzorua6@gmail.com>
2026-02-09 22:31:29 +0000

    added moving platform

---
+++ Assets/Scripts/Obstacles/MovingPlatform.cs
@@ -0,0 +1,31 @@
+using Mirror;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class MovingPlatform : NetworkBehaviour
+{
+    private Rigidbody _rb;
+    private SplineContainer _container;
+
+    private float _t = 0;
+    [SerializeField] private float _duration;
+
+
+    void Awake()
+    {
+        _rb = GetComponentInChildren<Rigidbody>();
+        _container = GetComponentInChildren<SplineContainer>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!isServer) { return; }
+
+        Vector3 localPos = _container.Splines[0].EvaluatePosition(_t);
+        Vector3 worldPos = _container.transform.TransformPoint(localPos);
+        _rb.MovePosition(worldPos);
+        _t = (_t + Time.fixedDeltaTime / _duration) % 1.0f;
+    }
+}
