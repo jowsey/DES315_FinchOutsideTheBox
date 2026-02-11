1309c13d12e30c83261bd7c0192bb32f4ecab330
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-02-11 02:02:49 +0000

    Ensure wheel radius is set outside in standalone build
    fixes #12

--- Assets/Scripts/WheelSeat.cs
+++ Assets/Scripts/WheelSeat.cs
@@ -27,9 +27,10 @@ public class WheelSeat : Mirror.NetworkBehaviour
 
     private PlayerController _seatedPlayer;
     
-    private float _radius;
-    private float _lastUnsitTime = -Mathf.Infinity;
+    [SerializeField] [Required] private SphereCollider _sphereCollider;
     
+    private float _lastUnsitTime = -Mathf.Infinity;
+
 
     [Mirror.Command(requiresAuthority = false)]
     public void CmdTrySitPlayer(Mirror.NetworkIdentity playerIdentity)
@@ -91,15 +92,18 @@ public class WheelSeat : Mirror.NetworkBehaviour
         {
             _wheelJoint.connectedBody = _cartRb;
         }
-
-        _radius = GetComponentInChildren<SphereCollider>().radius;
+        
+        if (!_sphereCollider)
+        {
+            _sphereCollider = GetComponentInChildren<SphereCollider>();
+        }
     }
 
     private void FixedUpdate()
     {
         if (!_seatedPlayer) return;
 
-        var wheelTop = transform.position + Vector3.up * (_radius * transform.lossyScale.y);
+        var wheelTop = transform.position + Vector3.up * (_sphereCollider.radius * transform.lossyScale.y);
 
         //Only apply force on server
         if (isServer)
@@ -113,6 +117,6 @@ public class WheelSeat : Mirror.NetworkBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(transform.position, _radius * transform.lossyScale.y);
+        Gizmos.DrawWireSphere(transform.position, _sphereCollider.radius * transform.lossyScale.y);
     }
 }
