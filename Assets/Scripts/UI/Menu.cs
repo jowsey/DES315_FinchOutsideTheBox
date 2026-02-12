e1cf2ec2fb28cf335a70f386626d72ba18f1d3ba
Thomas Jowsey <2302820@uad.ac.uk>
2026-02-12 14:09:09 +0000

    Join/host game buttons
    EOS is a stub and uses the native UI for now, need more time to figure it out. Sorry

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,9 +1,26 @@
+using EpicTransport;
+using kcp2k;
+using Mirror;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace UI
 {
     public class Menu : MonoBehaviour
     {
+        private NetworkManager _networkManager;
+
+        [SerializeField] [Required] private EosTransport _eosTransportPrefab;
+        [SerializeField] [Required] private KcpTransport _kcpTransportPrefab;
+
+        [SerializeField] [Required] private LobbyBrowser _lobbyBrowser;
+
+        private void Awake()
+        {
+            _networkManager = FindAnyObjectByType<NetworkManager>(FindObjectsInactive.Include);
+            _networkManager.gameObject.SetActive(false);
+        }
+
         public void Quit()
         {
 #if UNITY_EDITOR
@@ -15,12 +32,41 @@ namespace UI
 
         public void PlayOnline()
         {
-            Debug.Log("playing online yay");
+            Debug.Log("Playing online");
+
+            var transport = Instantiate(_eosTransportPrefab);
+            transport.GetComponent<EOSLobbyHUD>().manager = _networkManager;
+            DontDestroyOnLoad(transport);
+            _networkManager.transport = transport;
+            _networkManager.gameObject.SetActive(true);
+
+            // var eosLobby = transport.GetComponent<EOSLobby>();
+            // _lobbyBrowser.EosLobby = eosLobby;
+            // _lobbyBrowser.gameObject.SetActive(true);
         }
 
-        public void PlayLocal()
+        private void InitLocal()
         {
-            Debug.Log("playing locally die");
+            var transport = Instantiate(_kcpTransportPrefab);
+            DontDestroyOnLoad(transport);
+            _networkManager.transport = transport;
+            _networkManager.gameObject.SetActive(true);
+        }
+
+        public void HostLocal()
+        {
+            Debug.Log("Hosting new local game");
+            InitLocal();
+
+            _networkManager.StartHost();
+        }
+
+        public void JoinLocal()
+        {
+            Debug.Log("Joining local game");
+            InitLocal();
+
+            _networkManager.StartClient();
         }
     }
 }
