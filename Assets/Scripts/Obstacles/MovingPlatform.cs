e6dc016a265470862d35696a221a6162871a0301
Kaho Neki <shinyzorua6@gmail.com>
2026-02-23 07:00:45 +0000

    added animation curve to moving platform

--- Assets/Scripts/Obstacles/MovingPlatform.cs
+++ Assets/Scripts/Obstacles/MovingPlatform.cs
@@ -1,4 +1,5 @@
-using Mirror;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -8,6 +9,12 @@ public class MovingPlatform : MonoBehaviour
     private SplineContainer _container;
 
     [SerializeField] private float _duration;
+    [SerializeField] private AnimationCurve _displacementCurve;
+    [ShowInInspector, ReadOnly, ProgressBar(0, 1)] private float _currentT;
+
+    //Used to detect changes in _duration for calling ScaleEditorDisplacementCurve
+    private float _oldDuration = -1.0f;
+
 
     private void Awake()
     {
@@ -15,11 +22,58 @@ public class MovingPlatform : MonoBehaviour
         _container = GetComponentInChildren<SplineContainer>();
     }
 
+    private void OnValidate()
+    {
+        if (_duration != _oldDuration)
+        {
+            if (_oldDuration > 0.0f)
+            {
+                ScaleEditorDisplacementCurve();
+            }
+            _oldDuration = _duration;
+        }
+    }
+
+    //Called whenever _duration is changed
+    private void ScaleEditorDisplacementCurve()
+    {
+        //Animation curve needs to be scaled from [0, 1] (default) to [0, duration]
+        float timeScaleFactor = _duration / _oldDuration;
+        Keyframe[] keys = _displacementCurve.keys;
+        for (int i = 0; i < _displacementCurve.length; ++i)
+        {
+            keys[i].time *= timeScaleFactor;
+
+            //tangent = ds/dt, stretching t (time) by a factor, k, means tangent = ds/kdt, which means tangent needs to be divided by k
+            keys[i].inTangent /= timeScaleFactor;
+            keys[i].outTangent /= timeScaleFactor;
+        }
+        _displacementCurve.keys = keys;
+    }
+
     private void FixedUpdate()
     {
-        var t = (float)(NetworkTime.time % _duration / _duration);
-        Vector3 localPos = _container.Splines[0].EvaluatePosition(t);
+        //Range [0, _duration]
+        float absoluteT = (float)(Mirror.NetworkTime.time % _duration);
+
+        //Map the absolute t value to the splinal t value shaped by the _displacementCurve
+        //Range [0, 1]
+        _currentT = _displacementCurve.Evaluate(absoluteT);
+
+        //Evaluate spline
+        Vector3 localPos = _container.Splines[0].EvaluatePosition(_currentT);
         Vector3 worldPos = _container.transform.TransformPoint(localPos);
         _rb.MovePosition(worldPos);
     }
+
+    #if UNITY_EDITOR
+        [OnInspectorGUI]
+        private void RepaintConstantly()
+        {
+            if (Application.isPlaying)
+            {
+                GUIHelper.RequestRepaint();
+            }
+        }
+    #endif
 }
