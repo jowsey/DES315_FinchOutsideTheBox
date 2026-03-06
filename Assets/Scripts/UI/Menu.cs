8d8f2b332f9d9fabe6b885a8f382143bbafb0173
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-06 18:16:58 +0000

    Keep network transports permanently loaded
    Removes lag when pressing Play online, and allows Play online to be used multiple times in one session.

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -5,46 +5,57 @@ using Mirror;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 namespace UI
 {
     public class Menu : MonoBehaviour
     {
-        private NetworkManager _networkManager;
+        [SerializeField] [Required] private NetworkManager _networkManager;
 
         [SerializeField] [Required] private EosTransport _eosTransportPrefab;
         [SerializeField] [Required] private KcpTransport _kcpTransportPrefab;
 
-        [SerializeField] [Required] private LobbyBrowser _lobbyBrowser;
+        private static EosTransport _eosTransport;
+        private static KcpTransport _kcpTransport;
+
+        // [SerializeField] [Required] private LobbyBrowser _lobbyBrowser;
         [SerializeField] [Required] private MainMenuButton _settingsButton;
         [SerializeField] [Required] private SettingsManager _settings;
 
         [SerializeField] [Required] private InputActionReference _altMoveAction;
         [SerializeField] [Required] private InputActionReference _altJumpAction;
 
-
         private void Awake()
         {
-            _networkManager = FindAnyObjectByType<NetworkManager>(FindObjectsInactive.Include);
-            _networkManager.gameObject.SetActive(false);
             _settings.gameObject.SetActive(false);
-
-            // Clean up old transports if we're coming back from the game
+            Cursor.lockState = CursorLockMode.None;
+            
+            // Clean up previously-used transport if we're coming back from the game
             var transport = NetworkManager.singleton?.transport;
             if (transport is EosTransport eosTransport)
             {
                 // Leave Epic lobby if we're in one
                 eosTransport.GetComponent<EOSLobby>().LeaveLobby();
+                eosTransport.GetComponent<EOSLobbyHUD>().enabled = false;
             }
             else if (transport is KcpTransport kcpTransport)
             {
-                // not sure if we need to do anything here?   
+                // not sure if we need to do anything here?
             }
-            Destroy(transport?.gameObject);
 
-            Cursor.lockState = CursorLockMode.None;
-            
+            // Set up new transports if none exist
+            if (!_eosTransport)
+            {
+                _eosTransport = Instantiate(_eosTransportPrefab);
+                DontDestroyOnLoad(_eosTransport);
+            }
+
+            if (!_kcpTransport)
+            {
+                _kcpTransport = Instantiate(_kcpTransportPrefab);
+                DontDestroyOnLoad(_kcpTransport);
+            }
+
             // Reset player IDs before going into a new game
             PlayerController.NextPlayerNetworkId = 0;
         }
@@ -67,29 +78,17 @@ namespace UI
         {
             Debug.Log("Playing online");
 
-            if (_networkManager.transport != null)
-            {
-                // we currently offload to EOS built-in UI. eventually this will have proper state
-                Debug.Log("Existing transport found");
-                return;
-            }
+            var eosLobbyHUD = _eosTransport.GetComponent<EOSLobbyHUD>();
+            eosLobbyHUD.manager = _networkManager;
+            eosLobbyHUD.enabled = true;
 
-            var transport = Instantiate(_eosTransportPrefab);
-            transport.GetComponent<EOSLobbyHUD>().manager = _networkManager;
-            DontDestroyOnLoad(transport);
-            _networkManager.transport = transport;
+            _networkManager.transport = _eosTransport;
             _networkManager.gameObject.SetActive(true);
-
-            // var eosLobby = transport.GetComponent<EOSLobby>();
-            // _lobbyBrowser.EosLobby = eosLobby;
-            // _lobbyBrowser.gameObject.SetActive(true);
         }
 
         private void InitLocal()
         {
-            var transport = Instantiate(_kcpTransportPrefab);
-            DontDestroyOnLoad(transport);
-            _networkManager.transport = transport;
+            _networkManager.transport = _kcpTransport;
             _networkManager.gameObject.SetActive(true);
         }
 
@@ -107,12 +106,6 @@ namespace UI
             InitLocal();
 
             _networkManager.StartClient();
-        }
-
-        public void Settings()
-        {
-            _settings.gameObject.SetActive(!_settings.gameObject.activeSelf);
-            _settingsButton.SetActive(_settings.gameObject.activeSelf);
         }
 
         public void HostLocal2Player()
@@ -133,13 +126,19 @@ namespace UI
                 var otherPlayerController = otherPlayer.GetComponent<PlayerController>();
                 otherPlayerController.MoveAction = _altMoveAction;
                 otherPlayerController.JumpAction = _altJumpAction;
-                
+
                 NetworkServer.ReplacePlayerForConnection(
-                    NetworkServer.localConnection, 
+                    NetworkServer.localConnection,
                     otherPlayer,
                     ReplacePlayerOptions.KeepAuthority
                 );
             }
+        }
+
+        public void Settings()
+        {
+            _settings.gameObject.SetActive(!_settings.gameObject.activeSelf);
+            _settingsButton.SetActive(_settings.gameObject.activeSelf);
         }
 
         public void OpenBluesky() => Application.OpenURL("https://finchoutsidethebox.bsky.social");
