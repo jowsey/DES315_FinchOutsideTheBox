a8320e7ad6a38b100cf16abc6b4566fa02c8d21e
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-06 19:33:54 +0000

    Broad refactor/clean-up of cart, flask carrier, and checkpoint code
    - flasks are now handled in cart
    - checkpoints no longer store their index, resolved by cart

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Mirror;
+using Sirenix.OdinInspector;
 using UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,44 +9,83 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class Cart : NetworkBehaviour
 {
-    public Rigidbody Rb { get; private set; }
-    [SerializeField] private FlaskCarrier _flaskCarrier;
+    [ValidateInput("@$value.Count > 0", "Cart doesn't have any checkpoints linked.", InfoMessageType.Warning)]
+    [SerializeField] private List<Checkpoint> _checkpoints;
+    [field: SerializeField] public int CurrentCheckpointIndex { get; private set; }
 
-    public Checkpoint[] checkpoints;
-    [field: SerializeField] public int currentCheckpointIndex { get; private set; }
-
-    [SerializeField] private InputActionReference respawnAction;
-    [SerializeField] private InputActionReference dev_checkpointBackAction;
-    [SerializeField] private InputActionReference dev_checkpointForwardAction;
+    [SerializeField] [Required] private CheckpointBanner _checkpointBannerPrefab;
+    
+    [SerializeField] [Required] private InputActionReference _respawnAction;
+    [SerializeField] [Required] private InputActionReference _devCheckpointBackAction;
+    [SerializeField] [Required] private InputActionReference _devCheckpointForwardAction;
 
     [Tooltip("Whether to move the wheels using torque instead of flat forces. Should result in more consistent movement, but less tested.")]
     [field: SerializeField] [field: SyncVar] public bool UseNewTorqueSystem { get; private set; } = true;
-
-    [SerializeField] private CheckpointBanner _checkpointBannerPrefab;
     
-    private void Awake()
-    {
-        Rb = GetComponent<Rigidbody>();
-        _flaskCarrier = GetComponentInChildren<FlaskCarrier>();
-        
-        for (int i = 0; i < checkpoints.Length; i++)
-        {
-            checkpoints[i].index = i;
-            if (string.IsNullOrWhiteSpace(checkpoints[i].AreaName))
-            {
-                checkpoints[i].AreaName = $"Unnamed Checkpoint {i}";
-            }
-        }
-    }
+    // Flask carrying
+    [SerializeField] [Required] private Collider _flaskBounds;
+    
+    private Dictionary<GameObject, Vector3> _initialFlaskPositions = new();
+    
+    [field: SerializeField] [field: Mirror.ReadOnly] public int CarriedFlasks { get; private set; }
+    
+    [ValidateInput("@$value.Count > 0", "Cart doesn't have any flasks linked.", InfoMessageType.Warning)]
+    [SerializeField] private List<GameObject> _trackedFlasks = new();
+    
+    public int MaxFlasks => _trackedFlasks.Count;
+    
+    // Ratio of flasks currently being carried
+    public float FlasksRemainingRatio => (float)CarriedFlasks / _trackedFlasks.Count;
 
     private void Start()
     {
         Checkpoint.respawnEvent.AddListener(OnRespawn);
+        
+        foreach (var flask in _trackedFlasks)
+        {
+            _initialFlaskPositions[flask] = transform.InverseTransformPoint(flask.transform.position);
+        }
     }
     
     private void OnDestroy()
     {
         Checkpoint.respawnEvent.RemoveListener(OnRespawn);
+    }
+    
+    private void Update()
+    {
+        if (_respawnAction.action.WasPressedThisFrame())
+        {
+            CmdInvokeRespawnEvent(CurrentCheckpointIndex);
+        }
+        else if (_devCheckpointBackAction.action.WasPressedThisFrame() && CurrentCheckpointIndex != 0)
+        {
+            CmdInvokeRespawnEvent(CurrentCheckpointIndex - 1);
+        }
+        else if (_devCheckpointForwardAction.action.WasPressedThisFrame() && CurrentCheckpointIndex != _checkpoints.Count - 1)
+        {
+            CmdInvokeRespawnEvent(CurrentCheckpointIndex + 1);
+        }
+
+        // temp: debug, horrible, etc - swap between wheel force systems
+        if (Keyboard.current.tKey.wasPressedThisFrame && isServer)
+        {
+            UseNewTorqueSystem = !UseNewTorqueSystem;
+            Debug.Log($"UseNewTorqueSystem set to {UseNewTorqueSystem}");
+
+            var wheels = GetComponentsInChildren<WheelSeat>();
+            foreach (var wheel in wheels)
+            {
+                wheel.MoveForce *= !UseNewTorqueSystem ? 0.6f : 1 / 0.6f;
+            }
+        }
+        
+        // todo bad perf, should can probably just track in bounds trigger enter/exit
+        CarriedFlasks = _trackedFlasks.Count(f => _flaskBounds.bounds.Contains(f.transform.position));
+        if (isServer && CarriedFlasks == 0 && MaxFlasks > 0)
+        {
+            CmdInvokeRespawnEvent(CurrentCheckpointIndex);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -51,11 +93,13 @@ public class Cart : NetworkBehaviour
         if (other.CompareTag("Checkpoint"))
         {
             Checkpoint checkpoint = other.GetComponent<Checkpoint>();
-            Debug.Log($"Hit checkpoint {checkpoint.index}");
-            if (checkpoint.index > currentCheckpointIndex)
+            var newIndex = _checkpoints.IndexOf(checkpoint);
+            
+            if (newIndex > CurrentCheckpointIndex)
             {
                 // New checkpoint reached
-                currentCheckpointIndex = checkpoint.index;
+                CurrentCheckpointIndex = newIndex;
+                Debug.Log($"Hit checkpoint {newIndex}: {checkpoint.AreaName}");
 
                 var canvas = FindAnyObjectByType<Canvas>(); // todo we should probably have a global find object or similar for things like this. maybe tag it?
                 var checkpointBanner = Instantiate(_checkpointBannerPrefab, canvas.transform);
@@ -64,7 +108,7 @@ public class Cart : NetworkBehaviour
         }
     }
 
-    void OnRespawn(Checkpoint checkpoint)
+    private void OnRespawn(Checkpoint checkpoint)
     {
         Transform newTransform = checkpoint.cartRespawnLocalTransform;
         gameObject.SetActive(false);
@@ -79,38 +123,23 @@ public class Cart : NetworkBehaviour
         transform.rotation = newTransform.rotation;
         gameObject.SetActive(true);
         
-        if (_flaskCarrier) _flaskCarrier.ResetFlasks(true);
+        ResetFlasks(true);
     }
-
-    private void Update()
+    
+    public void ResetFlasks(bool includeOutOfBounds = false)
     {
-        if (respawnAction.action.WasPressedThisFrame())
+        foreach (var flask in _trackedFlasks)
         {
-            CmdInvokeRespawnEvent(currentCheckpointIndex);
-        }
-        else if (dev_checkpointBackAction.action.WasPressedThisFrame() && currentCheckpointIndex != 0)
-        {
-            CmdInvokeRespawnEvent(currentCheckpointIndex - 1);
-        }
-        else if (dev_checkpointForwardAction.action.WasPressedThisFrame() && currentCheckpointIndex != checkpoints.Length - 1)
-        {
-            CmdInvokeRespawnEvent(currentCheckpointIndex + 1);
-        }
-
-        // temp: debug, horrible, etc
-        if (Keyboard.current.tKey.wasPressedThisFrame && isServer)
-        {
-            UseNewTorqueSystem = !UseNewTorqueSystem;
-            Debug.Log($"UseNewTorqueSystem set to {UseNewTorqueSystem}");
-
-            var wheels = GetComponentsInChildren<WheelSeat>();
-            foreach (var wheel in wheels)
+            if (includeOutOfBounds || _flaskBounds.bounds.Contains(flask.transform.position))
             {
-                wheel.MoveForce *= !UseNewTorqueSystem ? 0.6f : 1 / 0.6f;
+                var rb = flask.GetComponent<Rigidbody>();
+                rb.position = transform.TransformPoint(_initialFlaskPositions[flask]);
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
             }
         }
     }
-
+    
     [Command(requiresAuthority = false)]
     public void CmdInvokeRespawnEvent(int newCheckpointIndex)
     {
@@ -120,10 +149,7 @@ public class Cart : NetworkBehaviour
     [ClientRpc]
     private void RpcInvokeRespawnEvent(int newCheckpointIndex)
     {
-        currentCheckpointIndex = newCheckpointIndex;
-        Checkpoint.respawnEvent.Invoke(checkpoints[currentCheckpointIndex]);
-        
-        // Test that reaching a checkpoint works when we get there
-        // Checkpoint.respawnEvent.Invoke(checkpoints[newCheckpointIndex]);
+        CurrentCheckpointIndex = newCheckpointIndex;
+        Checkpoint.respawnEvent.Invoke(_checkpoints[CurrentCheckpointIndex]);
     }
 }
--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,9 +5,8 @@ public class Checkpoint : MonoBehaviour
 {
     public static UnityEvent<Checkpoint> respawnEvent = new();
     
-    [HideInInspector] public int index;
     [field: SerializeField] public Transform[] playerRespawnLocalTransforms { get; private set; }
     [field: SerializeField] public Transform cartRespawnLocalTransform { get; private set; }
 
-    public string AreaName;
+    public string AreaName = "Unnamed Checkpoint";
 }
