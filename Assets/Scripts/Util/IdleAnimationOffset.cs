2d4255c3c245ed18b200f76ff1d0bd8106cca7e3
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-06 23:43:53 +0000

    Categorize some scripts into Util

---
+++ Assets/Scripts/Util/IdleAnimationOffset.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Util
+{
+    [RequireComponent(typeof(Animator))]
+    [InfoBox("Apply to a player to offset its initial idle animation by a random amount.")]
+    public class IdleAnimationOffset : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var animator = GetComponent<Animator>();
+            animator.Play("Idle", 0, Random.value);
+            animator.Update(0);
+        }
+    }
+}
