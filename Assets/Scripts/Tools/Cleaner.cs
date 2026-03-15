e375b3c8ecdef26bc7fec1dad9368002f1c71f9a
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-15 02:47:52 +0000

    Create cleaner tool for removing empty blockmesh objects
    watch this

---
+++ Assets/Scripts/Tools/Cleaner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Tools
+{
+    public class Cleaner : MonoBehaviour
+    {
+        [ShowInInspector] private int _numEmpty;
+
+        [Button("Count empty")]
+        public void CountEmpty()
+        {
+            _numEmpty = GetComponentsInChildren<Transform>()
+                .Count(child => child.childCount == 0 && child.GetComponents<Component>().Length == 1);
+        }
+
+        [Button("Cleanup empty in hierarchy")]
+        public void CleanEmpty()
+        {
+#if UNITY_EDITOR
+            var allTransforms = GetComponentsInChildren<Transform>();
+            
+            List<GameObject> markedForDeletion = new();
+
+            // If object has no children and no components, mark for deletion
+            foreach (var child in allTransforms)
+            {
+                if (child.childCount == 0 && child.GetComponents<Component>().Length == 1)
+                {
+                    markedForDeletion.Add(child.gameObject);
+                }
+            }
+
+            foreach (var obj in markedForDeletion)
+            {
+                UnityEditor.Undo.DestroyObjectImmediate(obj);
+            }
+#endif
+        }
+    }
+}
