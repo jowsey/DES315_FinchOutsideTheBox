a4121cf574c532ba6bc8701d6a147f9465751158
Orue-Dev <andrewgt687@gmail.com>
2026-03-22 16:04:55 +0000

    Obstacles Improvements

--- Assets/Scripts/Obstacles/TriggerZoneMove.cs
+++ Assets/Scripts/Obstacles/TriggerZoneMove.cs
@@ -5,12 +5,21 @@ public class TriggerZoneMovement : MonoBehaviour
 {
     [Tooltip("Triggerzone Start Event")]
     [SerializeField] private UnityEvent _triggerZoneEntered;
+    [SerializeField] private UnityEvent _triggerZoneExited;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player") || (other.gameObject.layer == LayerMask.NameToLayer("Cart")))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") || (other.gameObject.layer == LayerMask.NameToLayer("Cart")) || (other.gameObject.layer == LayerMask.NameToLayer("Interactable")))
         {
             _triggerZoneEntered.Invoke();
+        }
+    }
+
+    private void OnTriggerExited(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") || (other.gameObject.layer == LayerMask.NameToLayer("Cart")) || (other.gameObject.layer == LayerMask.NameToLayer("Interactable")))
+        {
+            _triggerZoneExited.Invoke();
         }
     }
 }
