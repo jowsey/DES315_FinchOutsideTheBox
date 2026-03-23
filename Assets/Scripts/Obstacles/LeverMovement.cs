2e09f2eb684bc984185764ef0e2342e129e59f41
Kaho Neki <shinyzorua6@gmail.com>
2026-03-23 20:23:25 +0000

    wrapped lever logic in authority checks

--- Assets/Scripts/Obstacles/LeverMovement.cs
+++ Assets/Scripts/Obstacles/LeverMovement.cs
@@ -31,48 +31,54 @@ public class LeverMovement : NetworkBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        _triggerColliding = true;
+        if (authority)
+        {
+            _triggerColliding = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
-        Debug.Log(_forward + " " + stateInfo.normalizedTime);
-        if ((stateInfo.normalizedTime >= 0.99f) && _forward)
+        if (authority)
         {
-            _onLeverTargetPos.Invoke();
+            AnimatorStateInfo stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+            Debug.Log(_forward + " " + stateInfo.normalizedTime);
+            if ((stateInfo.normalizedTime >= 0.99f) && _forward)
+            {
+                _onLeverTargetPos.Invoke();
 
-            //Clamp to 1 and stop animation so it doesn't go past 1
-            _animator.Play(stateInfo.fullPathHash, 0, 1.0f);
-            _animator.SetFloat("AnimSpeed", 0.0f);
-        }
-        else if ((stateInfo.normalizedTime <= 0.01f) && !_forward)
-        {
-            _onLeverDefaultPos.Invoke();
+                //Clamp to 1 and stop animation so it doesn't go past 1
+                _animator.Play(stateInfo.fullPathHash, 0, 1.0f);
+                _animator.SetFloat("AnimSpeed", 0.0f);
+            }
+            else if ((stateInfo.normalizedTime <= 0.01f) && !_forward)
+            {
+                _onLeverDefaultPos.Invoke();
 
-            //Clamp to 0 and stop animation so it doesn't go past 0 into negatives
-            _animator.Play(stateInfo.fullPathHash, 0, 0.0f);
-            _animator.SetFloat("AnimSpeed", 0.0f);
-        }
+                //Clamp to 0 and stop animation so it doesn't go past 0 into negatives
+                _animator.Play(stateInfo.fullPathHash, 0, 0.0f);
+                _animator.SetFloat("AnimSpeed", 0.0f);
+            }
 
-        //Check for changes in _triggerColliding state
-        if (!_triggerCollidingLastTick && _triggerColliding)
-        {
-            //Trigger is now active
-            _onLeverActivate.Invoke();
-            _animator.SetFloat("AnimSpeed", 1.0f);
-            _forward = true;
-        }
-        else if (_triggerCollidingLastTick && !_triggerColliding)
-        {
-            //Trigger is no longer active
-            _onLeverDeactivate.Invoke();
-            _animator.SetFloat("AnimSpeed", -1.0f);
-            _forward = false;
-        }
+            //Check for changes in _triggerColliding state
+            if (!_triggerCollidingLastTick && _triggerColliding)
+            {
+                //Trigger is now active
+                _onLeverActivate.Invoke();
+                _animator.SetFloat("AnimSpeed", 1.0f);
+                _forward = true;
+            }
+            else if (_triggerCollidingLastTick && !_triggerColliding)
+            {
+                //Trigger is no longer active
+                _onLeverDeactivate.Invoke();
+                _animator.SetFloat("AnimSpeed", -1.0f);
+                _forward = false;
+            }
 
-        //Reset
-        _triggerCollidingLastTick = _triggerColliding;
-        _triggerColliding = false;
+            //Reset
+            _triggerCollidingLastTick = _triggerColliding;
+            _triggerColliding = false;
+        }
     }
 }
