063ccb12f328a4e093154c20a47f6e003b682817
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-30 04:52:05 +0100

    Button feedback while creating/joining lobbies
    & lock to prevent pressing multiple times
    & reduce refresh countdown

---
+++ Assets/Scripts/UI/CreateLobbyButton.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CreateLobbyButton : MonoBehaviour
+    {
+        public string DefaultText { get; private set; }
+
+        [field: SerializeField] [field: Required] public Button Button { get; private set; }
+        [field: SerializeField] [field: Required] public TextMeshProUGUI LabelText { get; private set; }
+
+        [field: SerializeField] public string LoadingText { get; private set; } = "Creating...";
+
+        private void OnValidate()
+        {
+            if (!Button) Button = GetComponentInChildren<Button>();
+            if (!LabelText) LabelText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void Awake()
+        {
+            DefaultText = LabelText.text;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GloballyLockedButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace UI
+{
+    [InfoBox("Denotes a button that should globally lock and unlock based on game state")]
+    [RequireComponent(typeof(Button))]
+    public class GloballyLockedButton : MonoBehaviour
+    {
+        private static readonly HashSet<Object> _lockSources = new();
+
+        public static bool Locked => _lockSources.Count > 0;
+
+        public static void AddLockSource(Object source)
+        {
+            _lockSources.Add(source);
+            if (_lockSources.Count == 1) _onLock.Invoke();
+        }
+
+        public static void RemoveLockSource(Object source)
+        {
+            _lockSources.Remove(source);
+            if (_lockSources.Count == 0) _onUnlock.Invoke();
+        }
+
+        private static readonly UnityEvent _onLock = new();
+        private static readonly UnityEvent _onUnlock = new();
+
+        private Button _button;
+
+        private void OnEnable()
+        {
+            _button = GetComponent<Button>();
+            _onLock.AddListener(Lock);
+            _onUnlock.AddListener(Unlock);
+        }
+
+        private void OnDisable()
+        {
+            _onLock.RemoveListener(Lock);
+            _onUnlock.RemoveListener(Unlock);
+        }
+
+        private void Lock()
+        {
+            _button.interactable = false;
+        }
+
+        private void Unlock()
+        {
+            _button.interactable = true;
+        }
+    }
+}
