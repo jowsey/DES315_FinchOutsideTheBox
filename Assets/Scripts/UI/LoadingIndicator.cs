deeabf278ba9b7ca50b30d04a476af50c45b42fe
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-03-30 04:52:52 +0100

    Add Flask-san loading indicator prefab
    currently unused. will get around to it later for more "stuff is in progress" feedback

---
+++ Assets/Scripts/UI/LoadingIndicator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class LoadingIndicator : MonoBehaviour
+    {
+        public LoadingIndicator Instance { get; private set; }
+        
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Debug.LogWarning("Loading indicator already exists and you're making another. Are you Stupid?");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
