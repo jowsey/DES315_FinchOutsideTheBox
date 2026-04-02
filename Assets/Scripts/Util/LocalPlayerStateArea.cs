6c6755ab444b49b4a49413bf97c9f31849865e3a
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-04-02 05:55:16 +0100

    Fix non-local players being able to modify music state

---
+++ Assets/Scripts/Util/LocalPlayerStateArea.cs
@@ -0,0 +1,19 @@
+﻿using AK.Wwise;
+using Mirror;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Util
+{
+    [InfoBox("The selected state will be set when only the <b>local player</b> enters the bounds.")]
+    public class LocalPlayerStateArea : MonoBehaviour
+    {
+        [SerializeField] private State _state;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (NetworkClient.localPlayer.gameObject != other.attachedRigidbody.gameObject) return;
+            _state.SetValue();
+        }
+    }
+}
