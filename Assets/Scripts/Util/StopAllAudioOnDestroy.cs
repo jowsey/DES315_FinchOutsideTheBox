eda4d8f7093fa677b812563f708631ce554017bf
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-04-22 20:37:02 +0100

    Stop all Wwise audio on scene change
    Put it on WwiseGlobal and made it a prefab so it's consistent between scenes.

---
+++ Assets/Scripts/Util/StopAllAudioOnDestroy.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Util
+{
+    [InfoBox("Stops all Wwise audio when destroyed")]
+    public class StopAllAudioOnDestroy : MonoBehaviour
+    {
+        private void OnDestroy()
+        {
+            AkUnitySoundEngine.StopAll();
+        }
+    }
+}
