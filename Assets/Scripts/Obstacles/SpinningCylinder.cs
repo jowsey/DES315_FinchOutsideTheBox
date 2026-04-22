dd4b63543fd27740afafe0c27a4c1d6514dd12b6
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-04-22 19:00:32 +0100

    Remove empty method

--- Assets/Scripts/Obstacles/SpinningCylinder.cs
+++ Assets/Scripts/Obstacles/SpinningCylinder.cs
@@ -18,9 +18,4 @@ public class SpinningCylinder : MonoBehaviour
         Quaternion turnOffset = Quaternion.Euler(0, 0, _spinSpeed * Time.fixedDeltaTime);
         rb.MoveRotation(rb.rotation * turnOffset);
     }
-
-    private void OnDestroy()
-    {
-        
-    }
 }
