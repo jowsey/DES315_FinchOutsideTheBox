734545b38dcb5a0a3bab1e0d1061b69005ceba87
PAOLO SECHI <2302150@uad.ac.uk>
2026-07-02 15:13:43 +0100

    Emote Sfx

--- Assets/Scripts/Audio/WwiseAnimationEvents.cs
+++ Assets/Scripts/Audio/WwiseAnimationEvents.cs
@@ -7,6 +7,10 @@ public class WwiseAnimationEvents : MonoBehaviour
     [Required] public AK.Wwise.Event Footstep;
     [Required] public AK.Wwise.Event Jump;
     [Required] public AK.Wwise.Event Glide;
+    [Required] public AK.Wwise.Event headShake;
+    [Required] public AK.Wwise.Event headNod;
+    [Required] public AK.Wwise.Event spin; 
+    [Required] public AK.Wwise.Event Frontflip;
 
     [ReadOnly] public bool GlideTriggered;
     [ReadOnly] public bool DisableFootsteps;
@@ -36,5 +40,29 @@ public class WwiseAnimationEvents : MonoBehaviour
 
         Glide.Post(gameObject);
         GlideTriggered = true;
+    }
+
+    [UsedImplicitly]
+    public void HeadShake()
+    {
+        headShake.Post(gameObject);
+    }
+
+    [UsedImplicitly]
+    public void HeadNod()
+    {
+        headNod.Post(gameObject);
+    }
+
+    [UsedImplicitly]
+    public void PlaySpin()
+    {
+        spin.Post(gameObject);
+    }
+
+    [UsedImplicitly]
+    public void PlayFlipSound()
+    {
+        Frontflip.Post(gameObject);
     }
 }
