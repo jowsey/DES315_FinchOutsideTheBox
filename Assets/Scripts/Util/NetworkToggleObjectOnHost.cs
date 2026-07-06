9b9e24905eaac6da0fe36745143e855d35823065
Thomas Jowsey <2302820@uad.ac.uk>
2026-07-06 16:21:06 +0100

    Added new testing cart meshes

---
+++ Assets/Scripts/Util/NetworkToggleObjectOnHost.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Util
+{
+    public class NetworkToggleObjectOnHost : NetworkBehaviour
+    {
+        [SerializeField] private InputActionReference _toggleAction;
+        [SerializeField] private GameObject _object;
+        
+        private void Update()
+        {
+            if (!_object || !isServer) return;
+
+            if (_toggleAction.action.WasPressedThisFrame())
+            {
+                RpcToggle();
+            }
+        }
+
+        [ClientRpc]
+        private void RpcToggle()
+        {
+            _object.SetActive(!_object.activeSelf);
+        }
+    }
+}
