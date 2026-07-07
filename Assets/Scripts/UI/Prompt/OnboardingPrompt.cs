1bd5c829e3ab4d82b085be211c958e2c3ae06a35
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-07-07 20:13:44 +0100

    Add onboarding prompts

---
+++ Assets/Scripts/UI/Prompt/OnboardingPrompt.cs
@@ -0,0 +1,64 @@
+﻿using PrimeTween;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Event = AK.Wwise.Event;
+
+namespace UI
+{
+    public class OnboardingPrompt : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference _satisfyAction;
+        [SerializeField] private OnboardingPrompt _nextStep;
+
+        [SerializeField] private CanvasGroup _canvasGroup;
+
+        [SerializeField] private float _transitionDuration = 0.75f;
+        [SerializeField] private Event _satisfySfx;
+
+        private RectTransform _rt;
+
+        private bool _satisfied;
+
+        private void OnValidate()
+        {
+            if (!_canvasGroup) _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void Awake()
+        {
+            _rt = (RectTransform)transform;
+            _nextStep?.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (_satisfied || !_satisfyAction.action.WasPerformedThisFrame()) return;
+            _satisfied = true;
+
+            Tween.Delay(1.5f).OnComplete(() =>
+            {
+                Complete();
+
+                Tween.Delay(_transitionDuration * 2f).OnComplete(() =>
+                {
+                    if (_nextStep) _nextStep.gameObject.SetActive(true);
+                });
+            });
+        }
+
+        public void OnEnable()
+        {
+            Tween.Alpha(_canvasGroup, 0f, 1f, _transitionDuration, Ease.OutBack);
+            Tween.UIAnchoredPosition(_rt, _rt.anchoredPosition - _rt.sizeDelta * Vector2.up, _rt.anchoredPosition, _transitionDuration, Ease.OutBack);
+        }
+
+        private void Complete()
+        {
+            _satisfySfx?.Post(gameObject);
+
+            Tween.Alpha(_canvasGroup, 1f, 0f, _transitionDuration, Ease.InBack);
+            Tween.UIAnchoredPosition(_rt, _rt.anchoredPosition - _rt.sizeDelta * Vector2.up, _transitionDuration, Ease.InBack)
+                .OnComplete(() => gameObject.SetActive(false));
+        }
+    }
+}
