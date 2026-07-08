78ba0aa62923cde3ed302afbaa785be6f5291aec
Thomas Jowsey <2302820@uad.ac.uk>
2026-07-08 10:37:39 +0100

    Revert "Fix clicking triggering menu button FX in-game"
    It's no longer needed because Paolo changed the logic anyway in Music-Changes-07-07.

--- Assets/Scripts/UI/Menus/PauseMenu.cs
+++ Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -13,8 +13,6 @@ namespace UI
         [SerializeField] private RectTransform _hostLeaveDisbandWarning;
         [SerializeField] private TextMeshProUGUI _lobbyIdText;
 
-        [SerializeField] private EnsureSelection _ensureSelection;
-
         private bool _isActive;
         private CanvasGroup _canvasGroup;
 
@@ -78,7 +76,6 @@ namespace UI
         private void OnOpen(bool active)
         {
             _isActive = active;
-            if(_ensureSelection) _ensureSelection.enabled = active; // no need to ensure a UI selection if no main UI is open
 
             Cursor.lockState = active ? CursorLockMode.None : CursorLockMode.Locked;
             if (active)
