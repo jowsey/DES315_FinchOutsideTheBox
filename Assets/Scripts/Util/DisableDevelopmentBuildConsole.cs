42004a570d61ef3f1ed20cfbd0f934d1fd5df9a6
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-07-20 18:55:46 +0100

    Disable debug console in dev builds & some quick nitpicks

---
+++ Assets/Scripts/Util/DisableDevelopmentBuildConsole.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+    public static class DisableDevelopmentBuildConsole
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void DisableDeveloperConsole()
+        {
+            if (Debug.isDebugBuild) Debug.developerConsoleEnabled = false;
+        }
+    }
+}
