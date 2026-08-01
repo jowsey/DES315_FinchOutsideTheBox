bab541f6e05a6d0656bae1b1acc8916302dd28ed
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-08-01 22:28:31 +0100

    Import new sack assets
    unused

---
+++ Assets/Scripts/Game/UpgradeSack.cs
@@ -0,0 +1,20 @@
+﻿using Game.Items;
+using Mirror;
+using UnityEngine;
+
+namespace Game
+{
+    public class UpgradeSack : NetworkBehaviour
+    {
+        [SerializeField] private GameObject _emptySack;
+        [SerializeField] private GameObject _fullSack;
+
+        [field: SyncVar(hook = nameof(OnStoredItemChanged))] public Item StoredItem { get; private set; }
+
+        private void OnStoredItemChanged(Item oldValue, Item newValue)
+        {
+            _emptySack.SetActive(!newValue);
+            _fullSack.SetActive(newValue);
+        }
+    }
+}
