13b2f5ed36c1283ec0b02bfc0cd9885c4646bc00
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-08-03 09:06:14 +0100

    Fix windmill propellors in menu

---
+++ Assets/Scripts/Obstacles/SpinningCylinderNonSynced.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Obstacles
+{
+    [InfoBox("Identical behaviour to SpinningCylinder but is intentionally not synced over the network, for use in the menu and such")]
+    public class SpinningCylinderNonSynced : MonoBehaviour
+    {
+        [SuffixLabel("deg/s")]
+        [SerializeField] private float _spinSpeed;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            Quaternion totalRotation = Quaternion.Euler(0, 0, _spinSpeed * Time.fixedDeltaTime);
+            _rb.MoveRotation(_rb.rotation * totalRotation);
+        }
+    }
+}
