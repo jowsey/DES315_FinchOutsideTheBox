44dc788d23f85a1caf0d3550941e6b4604755ea5
Orue-Dev <andrewgt687@gmail.com>
2026-08-05 17:19:27 +0100

    Added Bounce Pads (For Ellis)

---
+++ Assets/Scripts/Obstacles/Pad.cs
@@ -0,0 +1,50 @@
+﻿using Mirror;
+using PrimeTween;
+using UnityEngine;
+using Event = AK.Wwise.Event;
+
+namespace Obstacles
+{
+    public class SucculentPad2 : MonoBehaviour
+    {
+        private const float TransitionDuration = 1.25f;
+
+        [SerializeField] private float _jumpForce;
+
+        [SerializeField] private Event _bounceSfx;
+        [SerializeField] private LayerMask _bounceLayers;
+
+        private float _lastBounceTime;
+        private float _bounceCooldown = 0.35f;
+
+        private void Start()
+        {
+            Tween.Scale(transform, Vector3.zero, transform.localScale, TransitionDuration, Ease.OutBounce);
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            if ((_bounceLayers.value & (1 << collision.gameObject.layer)) == 0) return;
+            
+            if (Time.time - _lastBounceTime < _bounceCooldown) return;
+            if (collision.rigidbody.isKinematic) return;
+
+            if (!collision.body.TryGetComponent(out NetworkTransformBase identity)) return;
+            if (!identity.authority) return;
+
+            _lastBounceTime = Time.time;
+            collision.rigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+
+            Bounce();
+        }
+
+        private void Bounce()
+        {
+            Sequence.Create()
+                .Chain(Tween.ScaleY(transform, 1.2f, 0.18f, Ease.InOutBack))
+                .Chain(Tween.ScaleY(transform, 1f, 0.16f, Ease.OutBack));
+
+            _bounceSfx?.Post(gameObject);
+        }
+    }
+}
