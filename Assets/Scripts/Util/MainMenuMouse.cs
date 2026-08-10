3516dcd177586bf4eab250cdee3685ff610dfb71
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-08-10 03:39:09 +0100

    Add little mouse 🐭
    he doesn't do much yet sadly. maybe for the showcase

---
+++ Assets/Scripts/Util/MainMenuMouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Util
+{
+    public class MainMenuMouse : MonoBehaviour, IPointerClickHandler
+    {
+        private static readonly int Run = Animator.StringToHash("Run");
+
+        [SerializeField] private Animator _animator;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _animator.SetTrigger(Run);
+        }
+    }
+}
