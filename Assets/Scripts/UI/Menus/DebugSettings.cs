20bce15185348452ece2601347dfd9098e99e333
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2026-09-02 23:45:26 +0100

    Add debug keys & starting checkpoint settings when in DEV_KEYS or editor
    hopefully they're fully hidden from player builds

---
+++ Assets/Scripts/UI/Menus/DebugSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Menus
+{
+#if DEV_KEYS || UNITY_EDITOR
+    public class DebugSettings : MonoBehaviour
+    {
+        [field: SerializeField] public Toggle EnableDebugKeysToggle { get; private set; }
+        [field: SerializeField] public Slider StartingCheckpointSlider { get; private set; }
+    }
+#endif
+}
